c672d1091e72c412e71d012ebb8cc1bf3fbfe709
yindan326 <yindan326@163.com>
2019-07-31 17:00:24 +0800

    opengl sample with glew

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication3.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+//typedef float TypeA[3];
+//int main()
+//{
+//	TypeA a;
+//	a[0] = 1;
+//	a[1] = 10;
+//	a[2] = 100;
+//
+//    return 0;
+//}
+
